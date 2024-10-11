39038afaecbd269d022e51e0aaee919c3401e0a2
Jaro95 <jmar95@o2.pl>
2024-10-11 22:40:25 +0200

    first commit

---
+++ src/main/java/pl/jaroslaw/bybetterperson/ByBetterPersonApplication.java
@@ -0,0 +1,13 @@
+package pl.jaroslaw.bybetterperson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ByBetterPersonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ByBetterPersonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/jaroslaw/bybetterperson/ByBetterPersonApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.jaroslaw.bybetterperson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ByBetterPersonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
